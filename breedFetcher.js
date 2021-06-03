2861ea6d245ae9d8717beee5ce7f5e625b64ad57
Yucen Liu <yucen.u@gmail.com>
2021-06-03 22:06:44 +0000

    made changes to breedFetcher.js/created index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,23 @@
 const request = require('request');
 
-const breedFetcher = (breedName, callback) => {
+const fetchBreedDescription = (breedName, callback) => {
   // const url = `https://api.thecatapi.com/v1/breeds/?${breedName}`;
 
-  request(`https://api.thecatapi.com/v1/breeds/?${breedName}`, (error, response, body) => {
-    //console.log(body);
-    //console.log(typeof body);
-
-    const data = JSON.parse(body);
-    // console.log(data);
-    // console.log(typeof data);
+  request(`https://api.thecatapi.com/v1/breeds/?q=${breedName}`, (error, response, body) => {
 
     if (error) {
-      return callback(error);
+      callback(error, null);
+    };
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      return callback('Invalid Input');
     } else {
-      const data = JSON.parse(body);
-      if (data.length === 0) {
-        return callback('Invalid Input');
-      } else {
-        return callback(null,data[0].description);
+       callback(null,data[0].description);
       }
-    }
-  
-  });
-};
-module.exports = {breedFetcher};
+    })
+   
+  };
+
+module.exports = {fetchBreedDescription};
---
+++ index.js
@@ -0,0 +1,13 @@
+// this file will be the one that users can run and provide the breed name TO
+
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
